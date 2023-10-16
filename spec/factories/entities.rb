6c266dff9da1baa3246e0ad06a377528e66b12f6
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-10-16 12:39:04 -0300

    Delete files with wrong name

--- spec/factories/entities.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :entity do
-    author_id { 1 }
-    name { 'MyString' }
-    amount { '9.99' }
-    created_at { '2023-09-20 17:36:13' }
-  end
-end
--- spec/factories/groups.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :group do
-    name { 'MyString' }
-    icon { 'MyString' }
-    created_at { '2023-09-20 17:36:25' }
-  end
-end
