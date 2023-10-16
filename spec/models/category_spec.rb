508a6d35ebbf22dc7c5fe5c11d894ce2082c5c76
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-10-16 13:54:18 -0300

    Add the tests fo r the category model

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:user) }
+
+  it { should validate_presence_of(:name) }
+
+  describe 'icon attachments' do
+    it 'should have icon attached' do
+      user = create(:user)
+      category = create(:category, user: user)
+
+      expect(category.icon).to be_attached
+    end
+  end
 end
