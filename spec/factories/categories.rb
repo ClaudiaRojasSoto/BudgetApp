9e329b8aaf5cad3d14b35f739156790d84605e80
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-10-16 13:56:04 -0300

    Modify the factory for categories

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -1,7 +1,14 @@
 FactoryBot.define do
-  factory :group do
-    name { 'MyString' }
-    icon { 'MyString' }
-    created_at { '2023-09-20 17:36:25' }
+  factory :category do
+    name { 'MyCategory' }
+    user
+
+    after(:build) do |category|
+      category.icon.attach(
+        io: File.open(Rails.root.join('spec', 'support', 'assets', 'test_icon.png')),
+        filename: 'test_icon.png',
+        content_type: 'image/png'
+      )
+    end
   end
 end
