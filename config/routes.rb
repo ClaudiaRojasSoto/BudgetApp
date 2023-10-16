8250e9416c079d7e03e26b81eb4faca24401ec44
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-10-16 09:46:55 -0300

    Change the routes nested on categories by independient routes for categories and purchases

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
-# config/routes.rb
 Rails.application.routes.draw do
   get 'home/index'
   get 'home/start'
@@ -7,7 +6,6 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :categories, only: %i[index new create edit update destroy]
-  resources :purchases, only: %i[index new create edit update destroy]
 
-  resources :purchase_categories, only: %i[index new create edit update destroy]
+  resources :purchases, only: %i[new create index edit update destroy]
 end
