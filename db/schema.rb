034af66ee699e5d58213020f9a8042a5a209ed95
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-10-16 09:45:53 -0300

    Upload the schema with the change on the migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_14_192659) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_16_014135) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -23,10 +51,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_14_192659) do
   end
 
   create_table "purchase_categories", force: :cascade do |t|
-    t.bigint "entity_id", null: false
-    t.bigint "group_id", null: false
-    t.index ["entity_id"], name: "index_purchase_categories_on_entity_id"
-    t.index ["group_id"], name: "index_purchase_categories_on_group_id"
+    t.bigint "purchase_id", null: false
+    t.bigint "category_id", null: false
+    t.index ["category_id"], name: "index_purchase_categories_on_category_id"
+    t.index ["purchase_id"], name: "index_purchase_categories_on_purchase_id"
   end
 
   create_table "purchases", force: :cascade do |t|
@@ -37,7 +65,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_14_192659) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
     t.string "email"
     t.string "encrypted_password"
     t.string "reset_password_token"
@@ -46,7 +73,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_14_192659) do
     t.string "full_name"
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "categories", "users"
-  add_foreign_key "purchase_categories", "categories", column: "group_id"
-  add_foreign_key "purchase_categories", "purchases", column: "entity_id"
+  add_foreign_key "purchase_categories", "categories"
+  add_foreign_key "purchase_categories", "purchases"
 end
