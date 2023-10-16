e00cb5dcfa3cad74c470268f65ee236fc80cc0d1
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-10-16 09:56:45 -0300

    Add all the methods for categories controlles to build new categories using the right names of the tables

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,9 +1,11 @@
 class CategoriesController < ApplicationController
   before_action :authenticate_user!
   before_action :set_category, only: %i[edit update destroy]
+  before_action :ensure_user_owns_category, only: %i[edit update destroy]
 
   def index
     @categories = current_user.categories
+    @total_purchases = Purchase.all.pluck(:amount).sum
   end
 
   def new
@@ -13,6 +15,7 @@ class CategoriesController < ApplicationController
   def create
     @category = current_user.categories.build(category_params)
     if @category.save
+      @categories = current_user.categories
       redirect_to categories_path, notice: 'Category was successfully created.'
     else
       render :new
@@ -37,10 +40,16 @@ class CategoriesController < ApplicationController
   private
 
   def set_category
-    @category = Category.find(params[:id]) 
+    @category = Category.find(params[:id])
   end
 
   def category_params
     params.require(:category).permit(:name, :icon)
+  end
+
+  def ensure_user_owns_category
+    return if current_user == @category.user
+
+    redirect_to categories_path, alert: 'You are not authorized to perform this action.'
   end
 end
