f6550935c191ada5ef5b8f440c604a75b6e4b94b
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-10-16 12:30:52 -0300

    Change the user factory adding the right parameters

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    name { 'MyString' }
+    full_name { 'John Doe' }
+    email { Faker::Internet.email }
+    password { 'password123' }
+    password_confirmation { 'password123' }
   end
 end
