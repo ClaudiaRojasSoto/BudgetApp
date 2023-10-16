def2d4f41a235f955e90d326f4c2e064fe9a2e0d
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-10-16 13:53:34 -0300

    Add the test for the purchase categories model

--- spec/models/purchase_category_spec.rb
+++ spec/models/purchase_category_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe PurchaseCategory, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:purchase) }
+  it { should belong_to(:category) }
 end
