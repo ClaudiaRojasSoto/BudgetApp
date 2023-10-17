d9f82b65e12b3c0a2ac34f1301fab8ee36362da8
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-10-17 14:24:45 -0300

    Delete comments from the code

--- spec/features/home_feature_spec.rb
+++ spec/features/home_feature_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 RSpec.feature 'Home', type: :feature do
   scenario 'User visits the home page' do
     visit root_path
-    expect(page).to have_content('BudgeTracker') # Adjust the content to match your actual home page
+    expect(page).to have_content('BudgeTracker')
   end
 
   scenario 'User visits the start page' do
     visit home_start_path
-    expect(page).to have_content('WELCOME!!') # Adjust the content to match your actual start page
+    expect(page).to have_content('WELCOME!!')
   end
 end
