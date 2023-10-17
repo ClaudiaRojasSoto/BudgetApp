b68c41b9437a0ccead08d027c7d7f9761c1f74ae
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-10-17 16:23:07 -0300

    Creates the user file for feature users

---
+++ spec/features/users_feature_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.feature 'User Authentication', type: :feature do
+  scenario 'User registers and logs in' do
+  visit new_user_registration_path
+
+  fill_in 'Full Name', with: 'John Doe'
+  fill_in 'user[email]', with: 'johndoe@example.com'
+  fill_in 'user[password]', with: 'password123'
+  fill_in 'user[password_confirmation]', with: 'password123'
+
+  click_button 'Sign up'
+
+  expect(current_path).to eq('/home/start')
+  end
+end
