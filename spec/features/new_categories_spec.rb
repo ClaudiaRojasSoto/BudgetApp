d462066f851644d6c8e15ff154c459a5da874b41
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-10-17 19:57:06 -0300

    Add the integration test to the view new category

---
+++ spec/features/new_categories_spec.rb
@@ -0,0 +1,30 @@
+RSpec.feature 'Categories Page', type: :feature do
+  let!(:user) { create(:user) }
+  let!(:categories) { create_list(:category, 3, user: user) }
+  let!(:total_purchases) { 0 }
+
+  before do
+    login_as(user, scope: :user)
+    visit categories_path
+  end
+
+  scenario "muestra el título 'CATEGORIES'" do
+    expect(page).to have_selector('h2#login-title', text: 'CATEGORIES')
+  end
+
+  scenario 'muestra el número total de compras de usuario' do
+    expect(page).to have_text('Gift credit')
+    expect(page).to have_text(total_purchases.to_s)
+  end
+
+  scenario 'muestra las categorías y sus totales' do
+    categories.each do |category|
+      expect(page).to have_selector('.category-container', text: category.name)
+      expect(page).to have_text("Total: $ #{category.category_total}")
+    end
+  end
+
+  scenario "contiene un enlace 'NEW CATEGORY'" do
+    expect(page).to have_link('NEW CATEGORY', href: new_category_path)
+  end
+end
