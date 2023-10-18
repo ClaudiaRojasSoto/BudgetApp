63449cceb4c9937f460732ef204c3313c3a85110
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-10-18 16:29:28 -0300

    establishing to true the enviroment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?
+  config.public_file_server.enabled = true
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
